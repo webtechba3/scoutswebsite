5b6cfba2f6b3058d6dc5348dc6d9832b90156141
Thibaut Beck <128092547+ThibautBeck1@users.noreply.github.com>
2024-11-06 12:27:45 +0100

    push it push it

--- routes/inschrijven.js
+++ routes/inschrijven.js
@@ -15,9 +15,7 @@ router.post('/verzendSignUpForm', (req, res) => {
     // Hier kun je de logica toevoegen om de gegevens te verwerken, zoals validatie of opslag in een database.
     console.log('Ontvangen gegevens:', { naam, email, woonplaats });
     // Voorbeeldvalidatie (aanpassen indien nodig)
-    if (!naam || !email || !woonplaats) {
-        return res.render('inschrijven', { message: 'Alle velden zijn verplicht.' });
-    }
+    
    
     // Optioneel: sla de gebruiker op in de database (als je een database gebruikt)
     // const newUser = new User({ naam, email, woonplaats });
@@ -26,7 +24,7 @@ router.post('/verzendSignUpForm', (req, res) => {
     //     .catch(err => res.status(500).json({ message: 'Er is een fout opgetreden.', error: err }));
 
     // Voor nu, laten we een succesbericht terugsturen
-    res.render('inschrijven', { message: 'sign up gelukt' }); // dit zorgt ervoor dat alles gecleared wordt
+    res.render('inschrijven'); // dit zorgt ervoor dat alles gecleared wordt
     
     
 });
