1c346334528485d29203edba83040248bcec77c7
Tano04 <164497733+Tano04@users.noreply.github.com>
2024-12-02 19:51:18 +0100

    Inloggen
    wachtwoorden worden nu wel controleren
    
    tabellen

--- routes/inloggen.js
+++ routes/inloggen.js
@@ -19,47 +19,42 @@ function requireCookies(req, res, next) {
 router.post('/verzendInlogForm', requireCookies, async (req, res) => {
   console.log('Sessie:', req.session);
   const { email, password } = req.body;
-  try{
+  try {
     const user = await req.app.locals.usersCollection.findOne({ email });
     if (user) {
-     
-    // Vergelijk het ingevoerde wachtwoord met het gehashte wachtwoord uit de database
-    // het gewone passwoord moet niet gehashed worden, terwijl user.password wel gehashed is
-    console.log(user.psswd , " user.password");
-    console.log(password , " password");
-    const password1 = await bcrypt.hash(password, 10)
-    console.log(password1 , " password1");
-    //if(user.psswd == password1){
-    
-    if( bcrypt.compare(password, user.psswd)){
-      console.log("ingelogd")   //Het is nodig om naar req.session te kijken omdat de sessie alle gebruikersspecifieke gegevens bevat die je gedurende de gebruikerssessie wilt bijhouden, zoals of de gebruiker een leiding is, wat hun voornaam en achternaam zijn, of welke tak ze hebben.
-      if (req.session) {
-        req.session.achternaam = user.naam; // Zorg ervoor dat je username in je database hebt
-        req.session.voornaam = user.voornaam;
-        req.session.tak = user.tak; 
-        req.session.role = "leiding" // geef die de rol van leiding
-        console.log(req.session , " sessie");
-        //res.locals.role = req.session ? req.session.role : null;
-        
-        req.session.save((err) => {
-          if (err) {
-            console.error("Fout bij opslaan van sessie:", err);
-            res.status(500).send("Fout bij het opslaan van sessie");
-          } else {
-            
-            res.redirect('/'); // Stuur door naar een andere pagina na het instellen van de sessie
-          }
-        });
-    }} else {
-      res.status(401).send("Ongeldige gebruikersnaam of wachtwoord");
+      console.log(user.psswd, " user.password in database");
+      console.log(password, " ingevoerd wachtwoord");
+      
+      // Gebruik bcrypt.compare om het ingevoerde wachtwoord te vergelijken met de gehashte waarde
+      const isMatch = await bcrypt.compare(password, user.psswd);
+      if (isMatch) {
+        console.log("Ingelogd"); 
+        if (req.session) {
+          req.session.achternaam = user.naam;
+          req.session.voornaam = user.voornaam;
+          req.session.tak = user.tak; 
+          req.session.role = "leiding";
+          console.log(req.session, " sessie");
+          
+          req.session.save((err) => {
+            if (err) {
+              console.error("Fout bij opslaan van sessie:", err);
+              res.status(500).send("Fout bij het opslaan van sessie");
+            } else {
+              res.redirect('/'); // Stuur door naar een andere pagina
+            }
+          });
+        }
+      } else {
+        res.status(401).send("Ongeldige gebruikersnaam of wachtwoord");
+      }
+    } else {
+      res.status(401).send("Gebruiker niet gevonden");
     }
+  } catch (error) {
+    console.error("Fout bij het controleren van gebruikersgegevens:", error);
+    res.status(500).send("Er is iets fout gegaan");
   }
-
-  // userCollection is de collection van de gebruikers
-}catch (error) {
-  console.error("Fout bij het controleren van gebruikersgegevens:", error);
-   // Fout tijdens controle
-}
-  
 });
+
 module.exports = router;
